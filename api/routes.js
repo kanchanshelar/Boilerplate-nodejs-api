fa081e5b5c798e364bf6bf64748ad931a3ed7d9c
Bismay Kumar Mohapatra <bismay4u@gmail.com>
2020-03-07 00:31:47 +0530

    Updated Debug Enviroment Configuration

--- api/routes.js
+++ api/routes.js
@@ -21,45 +21,69 @@ module.exports = function(server, restify) {
         res.sendRaw(server.config.welcome);
         return next();
     });
+    server.post('/', (req, res, next) => {
+        res.sendRaw(server.config.welcome);
+        return next();
+    });
 
     server.get('*', (req, res, next) => {
-        res.send("Not Found");
+        res.send(404);
+        return next();
+    });
+    server.post('*', (req, res, next) => {
+        res.send(404);
         return next();
     });
 
     server.get('/ping', (req, res, next) => {
-        res.header('content-type', 'json');
-        //res.header('timestamp', );
-        res.send({
-            "SERVER": server.config.name,
-            "VERSION": server.config.version,
-            "TIMESTAMP": new Date()
-        });
-        return next();
+        if(CONFIG.debug) {
+            res.header('content-type', 'json');
+            //res.header('timestamp', );
+            res.send({
+                "SERVER": server.config.name,
+                "VERSION": server.config.version,
+                "TIMESTAMP": new Date()
+            });
+            return next();
+        } else {
+            res.send(404);
+            return next();
+        }
     });
 
     server.get('/test', (req, res, next) => {
-        res.send({
-            "BODY": req.body,
-            "QUERY": req.query,
-            "PARAMS": req.params,
-            "HEADERS": req.headers,
-            //"GUID":req.get("GUID"),
-            //"DEVID":req.body.devid,
-        });
-        return next();
+        if(CONFIG.debug) {
+            res.send({
+                "BODY": req.body,
+                "QUERY": req.query,
+                "PARAMS": req.params,
+                "HEADERS": req.headers,
+                //"GUID":req.get("GUID"),
+                //"DEVID":req.body.devid,
+                "DEBUG": CONFIG.debug
+            });
+            return next();
+        } else {
+            res.send(404);
+            return next();
+        }
     });
 
     server.get('/routes', (req, res, next) => {
-        routeList = [];
-        _.each(server.router._registry._routes, function(a, b) {
-            if (a.method == "OPTIONS" || a.path == "*") return;
-            routeList.push({
-                "path": a.path,
-                "method": a.method,
+        if(CONFIG.debug) {
+            routeList = [];
+            _.each(server.router._registry._routes, function(a, b) {
+                if (a.method == "OPTIONS" || a.path == "*") return;
+                routeList.push({
+                    "path": a.path,
+                    "method": a.method,
+                });
             });
-        });
-        res.send(routeList);
-        next();
+            res.send(routeList);
+            next();
+        } else {
+            res.send(404);
+            return next();
+        }
     });
 }
